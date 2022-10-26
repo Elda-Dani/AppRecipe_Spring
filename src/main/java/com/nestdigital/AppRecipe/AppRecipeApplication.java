ffebfbcb01318cb089985dcc429bcb8efa30ab89
Elda-Dani <eldadani2001@gmail.com>
2022-10-26 15:41:24 +0530

    add

---
+++ src/main/java/com/nestdigital/AppRecipe/AppRecipeApplication.java
@@ -0,0 +1,13 @@
+package com.nestdigital.AppRecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppRecipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nestdigital/AppRecipe/AppRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestdigital.AppRecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppRecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
